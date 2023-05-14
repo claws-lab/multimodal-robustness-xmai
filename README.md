fba77bdf53ee54ac1338b59acd3ecfec2bcdeff8
Shivaen <shivaen.ramshetty@gmail.com>
2023-05-13 19:35:02 -0700

    Reorganize and add augmentation and eval scripts

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # XMAI for Multimodal Robustness
 ### Repository for ACL'23 Paper: "Cross-Modal Attribute Insertions for Assessing the Robustness of Vision-and-Language Learning"
+
+Shivaen Ramshetty*, Gaurav Verma*, Srijan Kumar
+
 We provide an easy to follow repository with guided notebooks detailing our baselines, method, and evaluation.
 
 ## Datasets and Preprocessed Data
@@ -9,48 +12,7 @@ The dataset subsets can be downloaded here:
 
 To allow for rapid experimentation we provide pre-computed objects and attributes for each dataset:
 - MSCOCO Validation 2017: [repo](processed_data/coco_objs_attrs_captions.csv)
-- SNLI-VE Test: Large File (will be shared after anonymity period) ***Necessary for evaluation***
-
-## Recommended Organization
-```bash
-XMAI
-├───colab_notebooks
-│   ├───augmentation
-│   │       TextAttack_baselines.ipynb
-│   │       XMAI.ipynb
-│   │
-│   └───evaluation
-│           CLIP_MSCOCO.ipynb
-│           METER_SNLI_VE.ipynb
-│
-├───images
-│       coco_val2017.zip
-│
-├───modified_captions
-│   ├───MSCOCO
-│   │   ├───baselines
-│   │   │       coco_check_modified.csv
-│   │   │       coco_clare_modified.csv
-│   │   │       coco_del_modified.csv
-│   │   │       coco_eda_modified.csv
-│   │   │
-│   │   └───XMAI
-│   └───SNLI_VE
-│       ├───baselines
-│       │       check_modified_hypothesis.csv
-│       │       clare_modified_hypothesis.csv
-│       │       del_modified_hypothesis.csv
-│       │       eda_modified_hypothesis.csv
-│       │
-│       └───XMAI
-├───object_detection
-│       augment_caption.ipynb
-│
-└───processed_data
-        coco_objs_attrs_captions.csv
-        snli_ve_test_objects_attrs.csv
-```
-
+- SNLI-VE Test: [gdrive](https://drive.google.com/file/d/1iBdsHi3UKIwKMyxV23gvgRnPS33FPwoa/view?usp=sharing)
 
 ## Object and Attribute Detection
 To perform object and attribute detection yourself:
@@ -59,16 +21,19 @@ To perform object and attribute detection yourself:
 3. Run `augment_caption.ipynb` within repo
 
 ## Augmentation
+To augment and evaluate your own data, we provide scripts in [XMAI](XMAI/)
+
+Notebooks and data for our paper are found within [paper_experimens](paper_experiments/)
 ### Baselines
-- The baseline notebook can be found here: [`colab_notebooks/augmentation/TextAttack_baselines.ipynb`](colab_notebooks/augmentation/TextAttack_baselines.ipynb)
-- We provide our resulting files under: [`modified_captions/`](modified_captions/)
+- The baseline notebook can be found here: [`paper_experiments/colab_notebooks/augmentation/TextAttack_baselines.ipynb`](paper_experiments/colab_notebooks/augmentation/TextAttack_baselines.ipynb)
+- We provide our resulting files under: [`paper_experiments/modified_captions/`](paper_experiments/modified_captions/)
 
 ### XMAI Method
-- The XMAI notebook can be found here: [`colab_notebooks/augmentation/TextAttack_baselines.ipynb`](colab_notebooks/augmentation/TextAttack_baselines.ipynb)
-- We provide our main experiment results under:  [`modified_captions/`](modified_captions/)
+- The XMAI notebook can be found here: [`paper_experiments/colab_notebooks/augmentation/TextAttack_baselines.ipynb`](paper_experiments/paper_experiments/colab_notebooks/augmentation/TextAttack_baselines.ipynb)
+- We provide our main experiment results under:  [`paper_experiments/modified_captions/`](modified_captions/)
 
 ### Evaluation
-- We provide seperate evaluation scripts for MSCOCO and SNLI-VE datasets in [`colab_notebooks/evaluation/`](colab_notebooks/evaluation/)
+- We provide seperate evaluation scripts for MSCOCO and SNLI-VE datasets in [`paper_experiments/colab_notebooks/evaluation/`](paper_experiments/colab_notebooks/evaluation/)
 
 ## Acknowledgements
 We also thank the authors and contributors of the following repositories:
---
+++ XMAI/evaluation.py
@@ -0,0 +1,172 @@
+from __future__ import division
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.translate import bleu
+from nltk.translate.bleu_score import SmoothingFunction
+from nltk.translate.meteor_score import meteor_score
+
+import os
+import argparse
+from io import BytesIO
+import base64
+import numpy as np
+import pandas as pd
+import torch
+from sentence_transformers import SentenceTransformer, util
+import clip
+from tqdm import tqdm
+from PIL import Image
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        '-i', '--input_csv',
+        type=str,
+        required=True,
+        help="Path to the input csv file"
+    )
+    parser.add_argument(
+        '-t', '--text_columns',
+        type=str,
+        default=["text", "augmented"],
+        nargs=2,
+        help="Name of CSV columns containing text inputs"
+    )
+    parser.add_argument(
+        '-f', '--file_column',
+        type=str,
+        default="file",
+        help="Name of CSV column containing paths or names of image files"
+    )
+    parser.add_argument(
+        '-d', '--image_dir',
+        type=str,
+        default=None,
+        help="Path to image dir, not necessary if csv contains full paths"
+    )
+
+    args = parser.parse_args()
+
+    assert os.path.isfile(args.input_csv)
+    assert args.image_dir is None or os.path.isdir(args.image_dir)
+
+    return args
+
+
+def avg_bleu(og_caps, mod_caps):
+    smoother = SmoothingFunction()
+
+    scores = []
+    for og, mod in zip(og_caps, mod_caps):
+        og = word_tokenize(og)
+        mod = word_tokenize(mod)
+        scores.append(bleu([og], mod, smoothing_function=smoother.method4, auto_reweigh=True))
+
+    return np.mean(scores)
+
+
+def avg_meteor(og_hyps, mod_hyps):
+    scores = []
+    for og, mod in zip(og_hyps, mod_hyps):
+        og = word_tokenize(og)
+        mod = word_tokenize(mod)
+        scores.append(meteor_score([og], mod, gamma=0.5))
+
+    return np.mean(scores)
+
+
+# Compute mean sentence similarity over all caption pairs
+def mean_relevancy(sent_model, original, augmented, batch_size=256):
+    rels = []
+    for i in range(0, len(original), batch_size):
+        with torch.no_grad():
+            og_embs = sent_model.encode(original[i:i+batch_size])
+            aug_embs = sent_model.encode(augmented[i:i+batch_size])
+
+        rels += torch.diag(util.cos_sim(og_embs, aug_embs)).tolist()
+    
+    # return mean and std
+    return np.mean(rels), np.std(rels)
+
+
+def build_features(clip_model, imgs, og_caps, mod_caps):
+    image_input = torch.tensor(np.stack(imgs)).cuda()
+    orig_text_tokens = clip.tokenize(["This is " + desc for desc in og_caps], truncate=True).cuda()
+    mod_text_tokens = clip.tokenize(["This is " + desc for desc in mod_caps], truncate=True).cuda()
+
+    with torch.no_grad():
+        image_features = clip_model.encode_image(image_input).float()
+        orig_text_features = clip_model.encode_text(orig_text_tokens).float()
+        mod_text_features = clip_model.encode_text(mod_text_tokens).float()
+
+    image_features /= image_features.norm(dim=-1, keepdim=True)
+    orig_text_features /= orig_text_features.norm(dim=-1, keepdim=True)
+    mod_text_features /= mod_text_features.norm(dim=-1, keepdim=True)
+
+    return image_features, orig_text_features, mod_text_features
+
+
+def batch_experiment(clip_model, preprocess, og_texts, aug_texts, files, im_dir, batch_size=1000):
+    img_og_rel = []
+    img_mod_rel = []
+    for i in tqdm(range(0, len(og_texts), batch_size)):
+        images = []
+        # Ignore batches that are not "full"
+        if len(og_texts) - i < batch_size:
+            continue
+        for filename in files[i:i+batch_size]:
+            try:
+                img = Image.open(os.path.join(im_dir, filename)).convert("RGB")
+            except :
+                try:
+                    img = Image.open(BytesIO(base64.urlsafe_b64decode(filename)))
+                except:
+                    print("ERROR: Can't find/read image when computing CLIP dissimilarity.")
+                    raise RuntimeError
+            images.append(preprocess(img))
+
+        original = og_texts[i:i+batch_size]
+        augmented = aug_texts[i:i+batch_size]
+
+        image_features, orig_text_features, mod_text_features = build_features(clip_model, images, original, augmented)
+        image_features = image_features
+        orig_text_features = orig_text_features
+        mod_text_features = mod_text_features
+
+        orig_image_sim = orig_text_features @ image_features.T
+        mod_image_sim = mod_text_features @ image_features.T
+        orig_image_sim = orig_image_sim.cpu().numpy()
+        mod_image_sim = mod_image_sim.cpu().numpy()
+
+        img_og_rel += list(np.diag(orig_image_sim))
+        img_mod_rel += list(np.diag(mod_image_sim))
+    
+    print(f"Original Caption Image Relevance - Mean: {np.mean(img_og_rel)}, Std: {np.std(img_og_rel)}")
+    print(f"Modified Caption Image Relevance - Mean: {np.mean(img_mod_rel)}, Std: {np.std(img_mod_rel)}")
+
+
+if __name__ == "__main__":
+    nltk.download('punkt')
+    nltk.download('wordnet')
+
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    args = parse_args()
+
+    sent_model = SentenceTransformer('all-mpnet-base-v2').to(device)
+    clip_model, preprocess = clip.load("ViT-B/32", device=device)
+
+    data_df = pd.read_csv(args.input_csv)
+
+    og_texts = data_df[args.text_columns[0]].to_list()
+    aug_texts = data_df[args.text_columns[1]].to_list()
+
+    print("Comparing original and augmented texts ...")
+    print("Mean BLEU scores:", avg_bleu(og_texts, aug_texts))
+    print("Mean METEOR scores:", avg_meteor(og_texts, aug_texts))
+    print("Mean sentence similarity:", mean_relevancy(sent_model, og_texts, aug_texts))
+
+    print("Comparing texts to images...")
+    files = data_df[args.file_column]
+    batch_experiment(clip_model, preprocess, og_texts, aug_texts, files, args.image_dir)
